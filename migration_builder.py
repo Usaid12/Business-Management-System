b51db1bf99366e06108dbc3d83e2331fe7c46f4f
Ammar-Asim-23 <ammarasim065@gmail.com>
2023-05-03 23:31:18 +0500

    migration builder fixed

--- migration_builder.py
+++ migration_builder.py
@@ -1,8 +1,7 @@
 with open('migration_no.txt','r') as hd:
     num = int(hd.read())
-file = open(f'migration_no_{num}.txt' , 'w') 
+file = open(f'./src/database/migrations/migration_no_{num}.ts' , 'w') 
 num += 1
 file.close()
 with open('migration_no.txt' ,'w') as hd:
     hd.write(str(num))
-    
